297f7c74fb75686829833f304c36f9eef35515a7
lhsantiago <luizh.pasin@gmail.com>
2022-12-17 13:05:52 -0300

    primeiro commit

---
+++ projeto/script.js
@@ -0,0 +1,44 @@
+const meds = [];
+
+function addMed() {
+    event.preventDefault();
+    let newMed = {
+        "medName": document.getElementById("fname").value,
+        "medQtd": document.getElementById("fqtd").value,
+        "medClass": document.getElementById("fclass").value,
+    }
+    meds.push(newMed);
+    renderTable()
+}
+
+function renderTable() {
+    let table = document.getElementById("dataTable");
+
+    table.innerHTML = `
+    <tr>
+        <th>ID</th>
+        <th>Medicamento</th>
+        <th>Quantidade</th>
+        <th>Classe de Medicamento</th>
+        <th>Remover</th>
+    </tr>`
+
+    for(let i=0; i < meds.length; i++) {
+        table.innerHTML = table.innerHTML + `
+        <tr>
+            <td>${i}</td>
+            <td>${meds[i].medName}</td>
+            <td>${meds[i].medQtd}</td>
+            <td>${meds[i].medClass}</td>
+            <td><img src="./img/lixeira.png" onclick="removeMed(${i})"></td>
+        </tr>
+        `
+    }
+}
+
+function removeMed(index) {
+    meds.splice(index, 1)
+    renderTable()
+}
+
+renderTable()
